4a3aca27a72ab1018f900986999b1058949bdbae
sarabastian <seb118@georgetown.edu>
2020-10-08 10:45:49 -0400

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,16 @@
+
+
+class Artist
+
+    attr_accessor :name
+
+    #def initialize(name = "kiki")
+        #@name = name
+    #end
+
+    #def name
+        #@name
+   # end
+
+end
+#artist = Artist.new(name)
---
+++ lib/song.rb
@@ -0,0 +1,13 @@
+
+
+class Song
+
+    attr_accessor :title, :artist
+    def song
+        @song = song
+        
+    end
+    
+end
+
+#kiki = Song.new("In My Feelings")
